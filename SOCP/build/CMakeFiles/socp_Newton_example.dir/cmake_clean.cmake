d01615fc546da9dcf7ad32a4b4c0de7477c1cd9a
wcl <2235476550@qq.com>
2024-04-06 21:42:36 +0800

    add SOCP compare

---
+++ SOCP/build/CMakeFiles/socp_Newton_example.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/socp_Newton_example.dir/example/socp_Newton_example.cpp.o"
+  "socp_Newton_example.pdb"
+  "socp_Newton_example"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/socp_Newton_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
